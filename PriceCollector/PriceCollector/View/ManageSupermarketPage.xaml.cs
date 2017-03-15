cfff172e062a1aa55f43aca60ec114a6b39a672e
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2017-03-15 20:13:04 -0300

    Implementações do cadastro de supermercado

---
+++ PriceCollector/PriceCollector/View/ManageSupermarketPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriceCollector.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PriceCollector.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManageSupermarketPage : ContentPage
+    {
+        private ViewModel.ManageSupermarketViewModel _viewModel;
+        public ManageSupermarketPage(Model.SupermarketsCompetitors supermarketsCompetitors)
+        {
+            InitializeComponent();
+            _viewModel = new ManageSupermarketViewModel(supermarketsCompetitors,this);
+            BindingContext = _viewModel;
+        }
+    }
+}
---
+++ PriceCollector/PriceCollector/ViewModel/ManageSupermarketViewModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Plugin.Toasts;
+using PriceCollector.Annotations;
+using PriceCollector.Api.WebAPI.SupermarketCompetitors;
+using PriceCollector.Model;
+using PriceCollector.View;
+using Xamarin.Forms;
+
+namespace PriceCollector.ViewModel
+{
+    public class ManageSupermarketViewModel : INotifyPropertyChanged
+    {
+
+        #region Fields
+
+        private int _id;
+        private string _name;
+        private string _street;
+        private string _number;
+        private string _neighborhood;
+        private string _city;
+        private ISupermarketCompetitorApi _supermarketApi;
+        private IToastNotificator _notificator;
+        private ManageSupermarketPage _manageSupermarketPage;
+
+        #endregion
+
+        #region Properties
+
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Street
+        {
+            get { return _street; }
+            set
+            {
+                if (value == _street) return;
+                _street = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Number
+        {
+            get { return _number; }
+            set
+            {
+                if (value == _number) return;
+                _number = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Neighborhood
+        {
+            get { return _neighborhood; }
+            set
+            {
+                if (value == _neighborhood) return;
+                _neighborhood = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (value == _city) return;
+                _city = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+        public ManageSupermarketViewModel(Model.SupermarketsCompetitors supermarketsCompetitors, View.ManageSupermarketPage manageSupermarketPage)
+        {
+            Task.Run(async () => { await LoadAsync(supermarketsCompetitors); });
+            _manageSupermarketPage = manageSupermarketPage;
+            _supermarketApi = DependencyService.Get<ISupermarketCompetitorApi>();
+            _notificator = DependencyService.Get<IToastNotificator>();
+        }
+
+        private async Task LoadAsync(SupermarketsCompetitors market)
+        {
+            try
+            {
+                ID = market.ID;
+                Name = market.Name;
+                Neighborhood = market.Neighborhood;
+                Number = market.Number;
+                Street = market.Street;
+                City = market.City;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                await _notificator.Notify(ToastNotificationType.Error, ":(",
+                    "Ocorreu um erro inesperado, por favor tente mais tarde.", TimeSpan.FromSeconds(3));
+                await _manageSupermarketPage.Navigation.PopAsync();
+            }
+        }
+
+        #region INotifyPropertyChangeImpl
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}

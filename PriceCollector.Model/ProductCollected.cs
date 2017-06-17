b156c9c9f9e8c48261294bcace41c4232bb2b034
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2017-06-17 16:05:08 -0300

    Corrigindo: Qaundo o aplicativo coletar um produto sem registro no banco de dados ou no caso de não ter rede pra pesquisar nome e demais informações, deve ser deixado o codigo de barras explicito no lugar do nome, nos cardviwes da tela inicial.

--- PriceCollector.Model/ProductCollected.cs
+++ PriceCollector.Model/ProductCollected.cs
@@ -15,7 +15,9 @@ namespace PriceCollector.Model
         public DateTime CollectDate { get; set; }
         public decimal PriceCurrent { get; set; }
         public decimal PriceCollected { get; set; }
-
-
+        /// <summary>
+        /// GAMBIARRA ALERT(FIXME): Essa propriedade é usada pra corrigir o comportamento correto da exibição do produto, no caso de ele não estar cadastrado no banco de dados do usuario ou o coletor não dispor de rede no momento da coleta, sendo assim, exibido o codigo de barras ao invés do nome do produto.
+        /// </summary>
+        public string ProductNameDisplayed => string.IsNullOrEmpty(ProductName) ? BarCode : ProductName;
     }
 }

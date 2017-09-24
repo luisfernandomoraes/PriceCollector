166e236a22a9c84fe926333ca1934b971233e01f
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2017-09-24 15:45:07 -0300

    sync

---
+++ PriceCollector/PriceCollector.Droid/LinkerPleaseInclude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Plugin.Toasts;
+
+namespace PriceCollector.Droid
+{
+    // This class is never actually executed, but when Xamarin linking is enabled it does how to ensure types and properties
+    // are preserved in the deployed app.
+
+    public class LinkerPleaseInclude
+    {
+        public void Include(IToastNotificator notificator)
+        {
+            notificator.Notify(ToastNotificationType.Success, String.Empty, String.Empty, TimeSpan.MinValue);
+        }
+    }
+}
